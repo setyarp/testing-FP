b36b856b444ce4e2e1f44fa0edebe591307c81f9
Setya Rizky Pradana <setyarizkypradana@gmail.com>
2022-11-30 20:56:56 +0700

    add cors and fix models

--- db/migrations/20221124095958-create-user.js
+++ db/migrations/20221124095958-create-user.js
@@ -32,7 +32,7 @@ module.exports = {
         type: Sequelize.STRING,
       },
       phoneNumber: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.CHAR(13),
       },
       image: {
         type: Sequelize.TEXT,
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
+const cors = require("cors");
 
 const register = require("../app/controllers/registerController");
 const login = require("../app/controllers/loginController");
 const { runValidation, registerValidation } = require("../validation");
 
+router.use(cors());
 router.post("/api/register", registerValidation, runValidation, register.register);
 router.post("/login", login.signin);
 
